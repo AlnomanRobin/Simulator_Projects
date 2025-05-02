c022a0c3e373341e66bb10e5e9c0dcc7f8a5723a
Alnoman Robin <robinalnoman@gmail.com>
2025-05-02 23:19:20 +0600

    01

---
+++ Motion_Detector.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+int Pirsensor = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  Pirsensor = analogRead(A0);
+  Serial.println(Pirsensor);
+  if (Pirsensor >= 100) {
+    digitalWrite(7, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(7, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(10, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
